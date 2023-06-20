fc2f71b03ad8d94cef37de6ac10254c5537a0f0f
Konstantin Ntokas <k.ntokas@fz-juelich.de>
2023-06-20 17:35:51 +0200

    rtd yaml updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,11 +22,11 @@ release = '1.0.0'
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
+templates_path = []
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
