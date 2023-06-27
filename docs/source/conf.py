b18a88d931e1fff6da3426831aa5fbf99b6484d6
Konstantin Ntokas <k.ntokas@fz-juelich.de>
2023-06-27 11:22:50 +0200

    version problems resolved

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../../src/norton_beer'))
 project = 'Norton-Beer apodization'
 copyright = '2023, Konstantin Ntokas'
 author = 'Konstantin Ntokas'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
